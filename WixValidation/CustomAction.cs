7d051178db4a0e377297237c4c3daaeece692e44
efratshefing <efrats@shefing.com>
2024-01-28 11:21:23 +0200

    invalid: message box, updated version

--- WixValidation/CustomAction.cs
+++ WixValidation/CustomAction.cs
@@ -13,42 +13,50 @@ namespace WixValidation
         {
             session.Log("Efrat## Begin CustomAction1");
 
-
             // Validate AccountKey
-            string accountKey = session["ACCOUNTKEY"];
-            if (string.IsNullOrEmpty(accountKey))
-            {
-                session.Log("Efrat## AccountKey is required.");
-                return ActionResult.Failure;
-            }
+            //string accountKey = session["ACCOUNTKEY"];
+            //if (string.IsNullOrEmpty(accountKey))
+            //{
+            //    Record record = new Record();
+            //    record.FormatString = "AccountKey is required.";
+            //    session.Message(InstallMessage.Error | (InstallMessage)MessageIcon.Error | (InstallMessage)MessageButtons.OK, record);
+            //    session.Log("Efrat## AccountKey is required.");
+            //    return ActionResult.Failure;
+            //}
 
             // Validate DeviceType
             string deviceType = session["DEVICETYPE"];
             HashSet<string> validDeviceTypes = new HashSet<string> { "Type1", "Type2", "Type3" };
             if (!validDeviceTypes.Contains(deviceType))
             {
+                Record record = new Record();
+                record.FormatString = $"Invalid DeviceType: {deviceType}";
+                session.Message(InstallMessage.Error | (InstallMessage)MessageIcon.Error | (InstallMessage)MessageButtons.OK, record);
                 session.Log($"Efrat## Invalid DeviceType: {deviceType}");
                 return ActionResult.Failure;
             }
-          
+
             // Validate ServiceUrl
             string serviceUrl = session["SERVICEURL"];
             if (!IsValidUrl(serviceUrl))
             {
+                Record record = new Record();
+                record.FormatString = $"Invalid ServiceUrl: {serviceUrl}";
+                session.Message(InstallMessage.Error | (InstallMessage)MessageIcon.Error | (InstallMessage)MessageButtons.OK, record);
                 session.Log($"Efrat## Invalid ServiceUrl: {serviceUrl}");
                 return ActionResult.Failure;
             }
 
             return ActionResult.Success;
-             }
+        }
 
-            private static bool IsValidUrl(string url)
-            {
-                // Simple regex for checking if it starts with http:// or https://
-                Regex urlRegex = new Regex("^https?://.*");
-                return urlRegex.IsMatch(url);
-            }
-    }
+        private static bool IsValidUrl(string url)
+        {
+            // Simple regex for checking if it starts with http:// or https://
+            Regex urlRegex = new Regex("^https?://.*");
+            return urlRegex.IsMatch(url);
+        }
 
     }
+}
 
